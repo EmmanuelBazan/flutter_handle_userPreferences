215e61ddca419ebad15baa8da6fe3c408d388563
EmmanuelBazan <emmanuelbazan99@gmail.com>
2023-08-07 12:48:12 -0600

    First commit

---
+++ lib/screens/settings.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({Key? key}) : super(key: key);
+
+  static const routeName = 'settings';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text('SettingsScreen'),
+      ),
+    );
+  }
+}
